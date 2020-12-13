6fe458a24b0fb603fbae87db2366cfa73377194a
Guifindor <42903599+Guifindor@users.noreply.github.com>
2020-12-13 02:27:01 -0500

    Order class and Status enumerator
    Added a class for representing customer orders, and a Status enumerator for indicating what their status is

---
+++ Restful-Service/src/main/java/payroll/Order.java
@@ -0,0 +1,75 @@
+package payroll;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//ORDER is not a valid name for a table, so we must rename it to CUSTOMER_ORDER
+@Table(name = "CUSTOMER_ORDER")
+public class Order {
+	
+	private @ID @GeneratedValue Long id;
+	
+	private String description;
+	private Status status;
+	
+	Order (){
+		
+	}
+	
+	Order(String description, Status status){
+		this.description = description;
+		this.status = status;
+	}
+	
+	public Long getID() {
+		return this.id;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public String getStatus() {
+		return this.status;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Order))
+			return false;
+		Order order = (Order) o;
+		
+		return Objects.equals(this.id, order.id) && Objects.equals(this.description, order.description)
+				&& this.status == order.status;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.id, this.description, this.status);
+	}
+	
+	@Override
+	public String toString() {
+		return "Order{" + "id=" + this.id + ", description='" + 
+				this.description + '\'' + ", status=" + this.status + '}';
+	}
+}
---
+++ Restful-Service/src/main/java/payroll/Status.java
@@ -0,0 +1,8 @@
+package payroll;
+
+//An enumerator for representing the status of an order
+public enum Status {
+	IN_PROGRESS,
+	COMPLETED,
+	CANCELLED
+}
