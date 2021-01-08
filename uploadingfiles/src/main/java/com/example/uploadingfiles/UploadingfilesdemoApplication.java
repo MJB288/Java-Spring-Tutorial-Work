8b174069b7dce1a56a2588c18e16aae418bb9a8d
Guifindor <42903599+Guifindor@users.noreply.github.com>
2021-01-08 17:15:12 -0500

    StorageProperties and Filesystem
    Changed the Application.java file to be configured for the project. Added FileSystemStorageService to implement the StorageSystem interface. Added the storage properties file.

--- uploadingfiles/src/main/java/com/example/uploadingfiles/UploadingfilesdemoApplication.java
+++ uploadingfiles/src/main/java/com/example/uploadingfiles/UploadingfilesdemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.uploadingfiles;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+import com.example.uploadingfiles.storage.StorageProperties;
+import com.example.uploadingfiles.storage.StorageService;
+
+@SpringBootApplication //This is a convienience annotation
+//It adds @Configuration - Tags the class as a source of bean definitions for the
+//application context
+//@EnableAutoConfiguration - add beans on the classpath settings, other beans 
+//@ComponentScan - look for other components, configurations, services in com/example
+@EnableConfigurationProperties(StorageProperties.class)
 public class UploadingfilesdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UploadingfilesdemoApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
